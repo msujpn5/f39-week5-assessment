23d3e229e52db7506935dc20d83f13e53f492ac4
Rex Holmes <holmesrex515@gmail.com>
2023-05-13 03:38:47 +0900

    getCities function added

--- server/controller.js
+++ server/controller.js
@@ -238,5 +238,34 @@ module.exports = {
         `)
         .then((dbRes) => res.status(200).send(dbRes[0]))
         .catch(err => console.log(err))
-    }
+    },
+
+    createCity: (req, res) => {
+        const {name, rating, countryId} = req.body
+        
+        sequelize.query(`
+            INSERT INTO
+                cities (name, rating, country_Id)
+            VALUES
+                ('${name}', ${rating}, ${countryId});
+        `)
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    getCities: (req, res) => {
+        sequelize.query(`
+            SELECT
+                cities.city_id,
+                cities.name AS city,
+                cities.rating,
+                countries.country_id,
+                countries.name AS country
+            FROM cities
+            JOIN countries
+            ON cities.country_id = countries.country_id;
+        `)
+        .then((dbRes) => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
 }
